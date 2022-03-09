8c209870008cc7a33c8ca0fcd8ce0fc67b8d980d
Nosirbek <68727555+Xakimov1610@users.noreply.github.com>
2022-03-09 14:07:10 +0500

    Update

--- identity/Program.cs
+++ identity/Program.cs
@@ -1,3 +1,4 @@
+using identity.Data;
 using identity.Entity;
 using Microsoft.AspNetCore.Identity;
 
@@ -5,8 +6,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddIdentity<User, IdentityRole<Guid>>(options =>
 {
-    
-});
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredLength = 6;
+
+    options.User.RequireUniqueEmail = true;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddIdentityServer()
+.AddDeveloperSigningCredential();
 
 builder.Services.AddControllersWithViews();
 
